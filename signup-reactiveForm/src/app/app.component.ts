c7d30652d85d7b425a66a455ad87bbd99c7790de
Arumuga priya <arumugapriya@smitiv.co>
2021-04-18 12:41:10 +0530

    reactive forms in angular

---
+++ signup-reactiveForm/src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component,OnInit } from '@angular/core';
+import {FormBuilder,FormGroup,Validators } from "@angular/forms";
+import {PasswordChecker} from "./custom-validators/password-checker"
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  registerForm:FormGroup;
+  submitted=false;
+
+  constructor(private formbuilder:FormBuilder ){}
+
+  title = 'signup-reactive';
+
+  
+  ngOnInit() {
+    this.registerForm = this.formbuilder.group({
+      firstName: ["", Validators.required],
+      lastName: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ["", Validators.required],
+      acceptTandC: [false, Validators.requiredTrue],
+    }, {
+      validators: PasswordChecker("password", "confirmPassword"),
+    });
+   
+  }
+
+  get h(){
+   return this.registerForm.controls
+  }
+
+  onSubmit(){
+    this.submitted=true;
+    if(this.registerForm.invalid){
+      return;
+    }
+
+    console.table(this.registerForm.value)
+    console.table(this.registerForm)
+    console.table(this.h.firstName.errors)
+    alert("success signup\n"+JSON.stringify(this.registerForm.value))
+  }
+
+  onReset(){
+    this.submitted=false;
+    this.registerForm.reset()
+  }
+}
